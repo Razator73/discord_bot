4ada574a6262be6121440c8ec64d66167849a25c
Ryan Scott <rscott@bestcompany.com>
2019-08-12 08:46:54 -0600

    Seperate some of the functions.

--- dice_bot.py
+++ dice_bot.py
@@ -4,102 +4,70 @@ import random
 import json
 
 
-client = discord.Client()
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    com_regex = re.compile(r'^/(\w+)\s*(.*)?')
-
-    if com_regex.search(message.content):
-        command, instructions = com_regex.search(message.content).groups()
-        instructions = instructions.lstrip() if instructions else instructions
-        msg = commands[command](instructions)
-        await client.send_message(message.channel, msg)
-
-
-@client.event
-async def on_ready():
-        print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
-        print('-------')
-
-
-def dice_roll(roll_string):
-    """ This takes a string in the form of typical DnD rolls (eg 2d8 or 1d20)
-        These rolls can take modifiers and specify whether the number of high
-        rolls that should be kept or tossed (eg 5d8+3 keep 3)"""
-
+def get_roll_vars(roll_string):
     dice_regex = re.compile(r"(\d*)d(\d+)([+-])?(\d*)(\s?keep\s?|\s?toss\s?|\s?drop\s?|[ktd])?"
                             r"(\d*)(?:(!)(?:\[(\d+)\])?)?(?:\s+([+-])\s+)?",
                             re.IGNORECASE | re.VERBOSE)
 
-    try:
-        # search_roll = dice_regex.search(roll_string)
-        # assert search_roll and search_roll.group(0) == roll_string, 'Roll not recognized'
-        rolls = dice_regex.findall(roll_string)
-        assert rolls, "Didn't recognize the roll, try again"
-    except AssertionError:
-        return "I didn't recognize \"{}\" as a valid roll".format(roll_string)
+    rolls = dice_regex.findall(roll_string)
 
-    all_rolled = []
-    for roll in rolls:
-        num_dice, sides, sign, mod, keep_toss, toss_num, exp, exp_times, next_sign = roll
-        keep_toss = keep_toss.strip()
-        int_mod = int(sign + mod) if sign and mod else 0
-        num_dice = int(num_dice) if num_dice else 1
-        toss_num = int(toss_num) if toss_num else 0
-        sides = int(sides) if sides else 10
-        exp_times = int(exp_times) if exp_times else 3
+    return rolls
 
-        try:
-            assert num_dice > toss_num, 'Can not toss/keep/drop more than the rolled number of dice'
-        except AssertionError:
-            return 'Invalid roll. Need to roll more dice than are kept/tossed/dropped.'
 
-        rolled = []
-        sum_rolled = []
-        for die in range(num_dice):
-            roll_num = random.randint(1, sides)
-            if exp and roll_num == sides:
-                exp_roll = [roll_num]
-                i = 0
-                roll_sum = roll_num
-                while roll_num == sides and i < exp_times:
-                    roll_num = random.randint(1, sides)
-                    exp_roll.append(roll_num)
-                    i += 1
-                    roll_sum += roll_num
-                rolled.append(exp_roll)
-                sum_rolled.append(roll_sum)
-            else:
-                rolled.append(roll_num)
-                sum_rolled.append(roll_num)
+def init_roll_vars(roll):
+    num_dice, sides, sign, mod, keep_toss, toss_num, exp, exp_times, next_sign = roll
+    keep_toss = keep_toss.strip()
+    int_mod = int(sign + mod) if sign and mod else 0
+    num_dice = int(num_dice) if num_dice else 1
+    toss_num = int(toss_num) if toss_num else 0
+    sides = int(sides) if sides else 10
+    exp_times = int(exp_times) if exp_times else 3
 
-        # Keep or toss the higher rolls.
-        discarded = []
-        if keep_toss and keep_toss.lower() in ('k', 'keep'):
-            for i in range(len(rolled) - toss_num):
-                discard_index = sum_rolled.index(min(sum_rolled))
-                discarded.append(rolled.pop(discard_index))
-                sum_rolled.pop(discard_index)
-        if keep_toss and keep_toss.lower() in ('t', 'toss'):
-            for i in range(toss_num):
-                discard_index = sum_rolled.index(max(sum_rolled))
-                discarded.append(rolled.pop(discard_index))
-                sum_rolled.pop(discard_index)
-        if keep_toss in ('d', 'drop'):
-            for i in range(toss_num):
-                discard_index = sum_rolled.index(min(sum_rolled))
-                discarded.append(rolled.pop(discard_index))
-                sum_rolled.pop(discard_index)
+    return num_dice, sides, sign, mod, int_mod, keep_toss, toss_num, exp, exp_times, next_sign
 
-        all_rolled.append((rolled, sum_rolled, discarded, mod, int_mod, sign, next_sign))
 
+def perform_roll(num_dice, sides, keep_toss, toss_num, exp, exp_times):
+    rolled = []
+    sum_rolled = []
+    for die in range(num_dice):
+        roll_num = random.randint(1, sides)
+        if exp and roll_num == sides:
+            exp_roll = [roll_num]
+            i = 0
+            roll_sum = roll_num
+            while roll_num == sides and i < exp_times:
+                roll_num = random.randint(1, sides)
+                exp_roll.append(roll_num)
+                i += 1
+                roll_sum += roll_num
+            rolled.append(exp_roll)
+            sum_rolled.append(roll_sum)
+        else:
+            rolled.append(roll_num)
+            sum_rolled.append(roll_num)
+
+    # Keep or toss the higher rolls.
+    discarded = []
+    if keep_toss and keep_toss.lower() in ('k', 'keep'):
+        for i in range(len(rolled) - toss_num):
+            discard_index = sum_rolled.index(min(sum_rolled))
+            discarded.append(rolled.pop(discard_index))
+            sum_rolled.pop(discard_index)
+    if keep_toss and keep_toss.lower() in ('t', 'toss'):
+        for i in range(toss_num):
+            discard_index = sum_rolled.index(max(sum_rolled))
+            discarded.append(rolled.pop(discard_index))
+            sum_rolled.pop(discard_index)
+    if keep_toss in ('d', 'drop'):
+        for i in range(toss_num):
+            discard_index = sum_rolled.index(min(sum_rolled))
+            discarded.append(rolled.pop(discard_index))
+            sum_rolled.pop(discard_index)
+
+    return rolled, sum_rolled, discarded
+
+
+def get_roll_string(all_rolled):
     # Print out the string that will show the sum of the roll.
     total = 0
     overall_sum_str = '[' if len(all_rolled) > 1 else ''
@@ -122,6 +90,31 @@ def dice_roll(roll_string):
     return overall_sum_str
 
 
+def dice_roll(roll_string):
+    """ This takes a string in the form of typical DnD rolls (eg 2d8 or 1d20)
+        These rolls can take modifiers and specify whether the number of high
+        rolls that should be kept or tossed (eg 5d8+3 keep 3)"""
+
+    rolls = get_roll_vars(roll_string)
+    try:
+        assert rolls, "Didn't recognize the roll, try again"
+    except AssertionError:
+        return "I didn't recognize \"{}\" as a valid roll".format(roll_string)
+    all_rolled = []
+    for roll in rolls:
+        num_dice, sides, sign, mod, int_mod, keep_toss, toss_num, exp, exp_times, next_sign = init_roll_vars(roll)
+
+        try:
+            assert num_dice > toss_num, 'Can not toss/keep/drop more than the rolled number of dice'
+        except AssertionError:
+            return 'Invalid roll. Need to roll more dice than are kept/tossed/dropped.'
+
+        rolled, sum_rolled, discarded = perform_roll(num_dice, sides, keep_toss, toss_num, exp, exp_times)
+        all_rolled.append((rolled, sum_rolled, discarded, mod, int_mod, sign, next_sign))
+
+    return get_roll_string(all_rolled)
+
+
 def bot_help(message):
     if message:
         pass
@@ -129,6 +122,31 @@ def bot_help(message):
 
 
 if __name__ == '__main__':
+    client = discord.Client()
+
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        com_regex = re.compile(r'^/(\w+)\s*(.*)?')
+
+        if com_regex.search(message.content):
+            command, instructions = com_regex.search(message.content).groups()
+            instructions = instructions.lstrip() if instructions else instructions
+            msg = commands[command](instructions)
+            await client.send_message(message.channel, msg)
+
+
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('-------')
+
+
     commands = {'roll': dice_roll,
                 'help': bot_help}
 
