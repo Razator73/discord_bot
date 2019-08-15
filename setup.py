8338d2fe7d67de2f2004ff438a2073a18aad8aec
Ryan Scott <rscott@bestcompany.com>
2019-08-14 18:20:17 -0600

    Add setup for pip install

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Minimal setup file for tasks project."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='discord_bot',
+    version='0.1.0',
+    license='proprietary',
+    description='Simple python bot for Discord servers',
+
+    author='Ryan Scott',
+    author_email='Please use pythontesting.net contact form.',
+    url='https://pragprog.com/book/bopytest',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+
+    # install_requires=['click', 'tinydb', 'six'],
+    # extras_require={'mongo': 'pymongo'},
+
+    # entry_points={
+    #     'console_scripts': [
+    #         'tasks = tasks.cli:tasks_cli',
+    #     ]
+    # },
+)
